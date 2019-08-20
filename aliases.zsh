3b55910d9d2bb7dab5a0742d724fcd667902e1ab
Jaime Mendoza <jaime.mendoza@aroundhome.de>
2019-08-20 18:22:27 +0200

    minor config adjustments

--- aliases.zsh
+++ aliases.zsh
@@ -7,6 +7,7 @@ alias code='cd ~/code/'
 alias home='cd ~/'
 
 # Nav Shortcuts
+alias .....='cd ../../../../'
 alias ....='cd ../../../'
 alias ...='cd ../../'
 alias ..='cd ../'
@@ -14,9 +15,9 @@ alias ..='cd ../'
 # Quick-Commands
 alias vpn='cd ~/ && sudo openvpn client.ovpn'
 alias update='pip install --upgrade .'
-alias ezsh='sudo subl ~/.zshrc'
+alias ezsh='subl ~/.zshrc'
 alias szsh='source ~/.zshrc'
-alias ealias='sudo subl ~/.oh-my-zsh/custom/aliases.zsh'
+alias ealias='subl ~/.oh-my-zsh/custom/aliases.zsh'
 alias salias='source ~/.oh-my-zsh/custom/aliases.zsh'
 alias spin='. venv/bin/activate'
 alias dspin='deactivate'
@@ -24,14 +25,16 @@ alias dspin='deactivate'
 # Command Shortcuts & Enhancements
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 # sleep 10; alert
+alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
+alias mv='mv -iv'                           # Preferred 'mv' implementation
+alias cp='cp -iv'                           # Preferred 'cp' implementation
 alias la='ls -A'							# List all files
 alias l='ls -CF'
-alias cp='cp -iv'                           # Preferred 'cp' implementation
-alias mv='mv -iv'                           # Preferred 'mv' implementation
-alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
 alias c='clear'
 
 # Git Shortcuts
+alias clone='git clone'
 alias pull='git pull'
 alias push='git push'
 alias gs='git status'
+
