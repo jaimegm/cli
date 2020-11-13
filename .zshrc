24ccc70bbb3a66215ac36f7f7092dbc8e0efc669
jaime <jaime.mendoza@infarm.com>
2020-11-13 14:23:05 +0100

    Updated config Nov 2020

--- .zshrc
+++ .zshrc
@@ -3,7 +3,8 @@ export LANG=en_US.UTF-8               # System Lanaguage
 
 # Path to your oh-my-zsh installation.
 export ZSH="/Users/jaime/.oh-my-zsh"
-PATH=$PATH:/usr/local/bin/; export PATH
+#export CLOUDSDK_PYTHON=$(which python2)
+#PATH=$PATH:/usr/local/bin/; export PATH
 
 ##### THEME Config #######
 ZSH_THEME="powerlevel10k/powerlevel10k"
@@ -55,7 +56,7 @@ HISTFILE=~/.zsh_history     #  Where to save history to disk
 SAVEHIST=10000              #  Number of history entries to save to disk
 HISTDUP=erase               #  Erase duplicates in the history file
 setopt appendhistory        #  Append history
-setopt sharehistory         #  Share history across terminals
+#setopt sharehistory         #  Share history across terminals
 setopt incappendhistory     #  Immediate append
 setopt extendedhistory      #  Attach timestamps
 
@@ -129,11 +130,6 @@ export GOOGLE_APPLICATION_CREDENTIALS=/Users/jaime/creds/infarm-data-airflow-c22
 # Data directory to save results to
 export DATA_DIR=/Users/jaime/code/playground
 
-# Pyenv build configs
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-
-
 export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
 export LDFLAGS="-L/usr/local/opt/readline/lib"
@@ -141,3 +137,17 @@ export CPPFLAGS="-I/usr/local/opt/readline/include"
 export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
 export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
 eval "$(pyenv init -)"
+
+# Added by serverless binary installer
+export PATH="$HOME/.serverless/bin:$PATH"
+
+# tabtab source for packages
+# uninstall by removing these lines
+[[ -f ~/.config/tabtab/__tabtab.zsh ]] && . ~/.config/tabtab/__tabtab.zsh || true
+export PATH="/usr/local/opt/postgresql@11/bin:$PATH"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/jaime/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/jaime/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/jaime/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/jaime/google-cloud-sdk/completion.zsh.inc'; fi
--- aliases.zsh
+++ aliases.zsh
@@ -2,10 +2,13 @@
 ####--------------------####
 
 # Directory Shortcuts
+alias cloud='cd ~/code/cloud_function_dev'
 alias air='cd ~/code/grow_with_the_flow'
-alias dags='cd ~/code/infarm-dags'
+alias dags='cd ~/code/infarm_dags'
 alias play='cd ~/code/playground'
+alias proj='cd ~/code/projects'
 alias ref='cd ~/code/reference'
+alias kosmos='cd ~/code/kosmos'
 alias docs='cd ~/Documents'
 alias down='cd ~/Downloads'
 alias desk='cd ~/Desktop'
@@ -13,6 +16,13 @@ alias sql='cd ~/code/sql'
 alias creds='cd ~/creds'
 alias code='cd ~/code/'
 alias home='cd ~/'
+
+# Chrome
+alias chrome='open /Applications/Google\ Chrome.app'
+alias maps='open --new -a "Google Chrome" --args https://www.google.com/maps'
+alias drive='open --new -a "Google Chrome" --args https://drive.google.com/drive/u/0/my-drive'
+alias cal='open --new -a "Google Chrome" --args https://calendar.google.com/calendar/u/0/r'
+#https://zwbetz.com/open-a-new-google-chrome-tab-or-window-via-command-line-on-mac/
 
 # Nav Shortcuts
 alias ......='cd ../../../../../'
@@ -22,34 +32,53 @@ alias ...='cd ../../'
 alias ..='cd ../'
 
 # Additional Nav Shortcuts!
+#alias pyclean='find . | grep -E '(__pycache__|\.pyc|\.pyo$)' | xargs rm -rf'
+alias flushdns='sudo killall -HUP mDNSResponder; sleep 2;'
 alias mycode='cd /Volumes/Kr8tv/Users/Jaime/code'
 alias me='cd /Volumes/Kr8tv/Users/Jaime/Desktop'
+alias resetpyenv='pyenv local --unset'
+alias setpyenv='pyenv local'
+
+# BigQuery API Shortcuts
+alias bqshow='bq show --format=prettyjson'
+alias datasets='bq ls --format=pretty'
+
+# GCP Cli Shortcuts
+alias airreset='gcloud compute instances reset grow-with-the-flow'
+alias switch='gcloud config configurations activate'
+alias computes='gcloud compute instances list'
+alias gconfigs='gcloud config configurations list'
+alias gwtf='grow-with-the-flow'
 
 # Quick-Commands
+alias stoppg='launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist'
+alias startpg='launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist'
+alias update='pip install --upgrade . --use-feature=2020-resolver'
+alias fixpsql='rm /usr/local/var/postgres/postmaster.pid'
 alias sa='source ~/.oh-my-zsh/custom/aliases.zsh'
 alias ea='subl ~/.oh-my-zsh/custom/aliases.zsh'
-alias update='pip install --upgrade .'
+alias pgen='openssl rand -base64 20'
+alias mkvenv='python3 -m venv venv'
 alias spin='. venv/bin/activate'
 alias jup='jupyter notebook'
 alias szsh='source ~/.zshrc'
 alias ezsh='subl ~/.zshrc'
 alias dspin='deactivate'
-alias pgen='openssl rand -base64 20'
-alias mkvenv='python3 -m venv venv'
 
 # Command Shortcuts & Enhancements
-alias la='ls -A'                                 # List all files
-alias l='ls -CF'
-alias cp='sudo cp -iv'                           # Preferred 'cp' implementation
-alias mv='sudo mv -iv'                           # Preferred 'mv' implementation
-alias mkdir='sudo mkdir -pv'                     # Preferred 'mkdir' implementation
+alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
+alias mv='sudo mv -iv'                      # Preferred 'mv' implementation
 alias rmv='sudo rm -r'
+alias cp='sudo cp -iv'                      # Preferred 'cp' implementation
+alias l='ls -CF'
+alias la='ls -A'                            # List all files
 alias c='clear'
-alias startpg="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
-alias stoppg="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+alias copy='pbcopy'
+alias p='pbpaste'
 
 # Git Shortcuts
 alias commit='git commit -m'
+alias samecommit='git commit --amend --no-edit'
 alias clone='git clone'
 alias gc='git checkout'
 alias add='git add'
@@ -60,9 +89,16 @@ alias stash='git stash'
 alias unstash='git stash pop'
 alias nbranch='git checkout -b'
 alias pbranch='git push --set-upstream origin'
-alias submodules='git config --file .gitmodules --get-regexp path | awk "{ print $2 }"'
+alias rprune='git remote prune origin'
+#alias localprune='git branch --merged | egrep -v '(^\*|master|dev)' | xargs git branch -d'
+alias submodules='git config --file .gitmodules --get-regexp path | awk '{ print $2 }''
 
 # Airflow Shortcuts
-alias growmachine='ssh -p 22 jaime@34.89.145.127'
+alias restart='service airflow-webserver stop && service airflow-schedluer stop && git pull && sudo -H pip install --upgrade . && service airflow-webserver start && service airflow-schedluer start'
+alias start='service airflow-webserver start && service airflow-schedluer start'
+alias stop='service airflow-webserver stop && service airflow-schedluer stop'
+alias growmachine='ssh -p 49 jaime.mendoza@34.89.145.127'
+alias dev='ssh -p 22 jaime.mendoza@35.242.240.92'
 alias aweb='airflow webserver'
 alias sch='airflow scheduler'
+
